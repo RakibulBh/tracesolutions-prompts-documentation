b6db9f200aa0a37722595493d9064eb10ff1713d
Johann Cardona <2007048@brunel.ac.uk>
2025-03-14 15:12:58 +0000

    Update navlink.tsx

--- components/navlink.tsx
+++ components/navlink.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { BookOpen, Bot, ChevronRight, Cog } from "lucide-react";
+import { BookOpen, ChevronRight, Cog } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
